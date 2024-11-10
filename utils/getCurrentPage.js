d481d28bfbde3bc53d90731cfc88cd28d2d8a43e
Daniel <142172231+danielquirozc@users.noreply.github.com>
2024-11-09 23:36:44 -0400

    🌐: Solve error

--- utils/getCurrentPage.js
+++ utils/getCurrentPage.js
@@ -1,5 +1,5 @@
 export const getCurrentPage = () => {
   const path = window.location.pathname;
   const page = path.split('/').pop().split('.').shift(); // Extrae el nombre de la página
-  return page === 'index' ? 'home' : page;
+  return page === 'index' || '' ? 'home' : page;
 };
